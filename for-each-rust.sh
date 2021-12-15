d26ca5c1e9a8ed66968db8b7314b939b0deaccec
Michael Kirk <michael.code@endoftheworl.de>
2021-12-14 16:27:05 -0800

    fancy output

--- for-each-rust.sh
+++ for-each-rust.sh
@@ -1,16 +1,25 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
-pids=()
+declare -A pids
+declare -A statuses
 
 # for every version of rust we support...
-for rust_dir in rust-* 
-do   
+for rust_dir in rust-*
+do
     # run the given command in that rust-dir in parallel
     (cd $rust_dir && $@)&
-    pids+=($!)
-done  
+    pids[$rust_dir]="$!"
+done
 
-for pid in "${pids[@]}"; do
-    wait "$pid"
-    echo "waited pid: $pid"
+echo pids $pids
+for key pid in "${(@kv)pids}"
+do
+    wait $pid
+    echo "finished $key (pid: $pid)"
+    statuses[$key]=$?
+done
+
+for key val in "${(@kv)statuses}"
+do
+    echo "$key completed with status: $val"
 done
